fd5e7b9131be51769e78f603aeae4f7d2a172fc1
José <josecnrn@gmail.com>
2019-08-31 13:56:30 -0300

    Refatorando projeto
    Limpeza de projeto e adição do .gitignore (Não deverá mais subir arquivos do quartus pro remote) pra melhorar integração de desenvolvimento no ambiente do Quartus II.

--- quartus/simulation/qsim/work/rv32i_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity rv32i_vlg_check_tst is
-    port(
-        rs1             : in     vl_logic_vector(31 downto 0);
-        rs2             : in     vl_logic_vector(31 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end rv32i_vlg_check_tst;
