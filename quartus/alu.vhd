4945203b12a01304ec13b2eeaf1e171718c3e685
José <josecnrn@gmail.com>
2019-09-09 10:29:01 -0300

    Correções SLL e implementação do SRL e SRA.

--- quartus/alu.vhd
+++ quartus/alu.vhd
@@ -14,32 +14,55 @@ END alu;
 ARCHITECTURE comportamento OF alu IS
 
 	BEGIN
-	PROCESS(sel_alu)
-	VARIABLE i : integer;
-	VARIABLE in1_alu_temp : STD_LOGIC_VECTOR(31 DOWNTO 0);
-	
-	BEGIN
-	--aritimetica--
-		CASE sel_alu IS
-			WHEN "0000" =>
-				out_alu <= 	in1_alu + in2_alu; --ADD 	
-			WHEN "1000" => 
-				out_alu <= in1_alu - in2_alu;	 --SUB						
-	--logica-- 
-			WHEN "0100" => 
-				out_alu <= in1_alu XOR in2_alu; -- XOR
-			WHEN "0110" => 
-				out_alu <= in1_alu OR  in2_alu; -- OR
-			WHEN "0111" => 
-				out_alu <= in1_alu AND in2_alu; -- AND
-			WHEN "0001" =>
-			i := 0;
-				WHILE i <= TO_INTEGER(IEEE.NUMERIC_STD.UNSIGNED(in2_alu(4 DOWNTO 0))) LOOP
-					in1_alu_temp := in1_alu;
-					out_alu(31 DOWNTO 0) <= in1_alu_temp(30 DOWNTO 0) & '0';
-					i := i + 1;
-				END LOOP;
-			WHEN OTHERS => NULL;
-		END CASE;
-	END PROCESS;
+		PROCESS(sel_alu)
+		VARIABLE i : integer;
+		VARIABLE in1_alu_temp : STD_LOGIC_VECTOR(31 DOWNTO 0);
+		BEGIN
+		--aritimetica--
+			CASE sel_alu IS
+				WHEN "0000" =>
+					out_alu <= 	in1_alu + in2_alu; --ADD 	
+					
+				WHEN "1000" => 
+					out_alu <= in1_alu - in2_alu;	 --SUB	
+					
+		--logica-- 
+				WHEN "0100" => 
+					out_alu <= in1_alu XOR in2_alu; -- XOR
+					
+				WHEN "0110" => 
+					out_alu <= in1_alu OR  in2_alu; -- OR
+					
+				WHEN "0111" => 
+					out_alu <= in1_alu AND in2_alu; -- AND
+					
+				WHEN "0001" => --SLL
+				i := 0;
+				in1_alu_temp := in1_alu;
+					WHILE i < TO_INTEGER(IEEE.NUMERIC_STD.UNSIGNED(in2_alu(4 DOWNTO 0))) LOOP
+						in1_alu_temp(31 DOWNTO 0) := in1_alu_temp(30 DOWNTO 0) & '0';
+						i := i + 1;
+					END LOOP;
+				out_alu <= in1_alu_temp;
+				
+				WHEN "0101" => --SRL
+				i := 0;
+				in1_alu_temp := in1_alu;
+					WHILE i < TO_INTEGER(IEEE.NUMERIC_STD.UNSIGNED(in2_alu(4 DOWNTO 0))) LOOP
+						in1_alu_temp(31 DOWNTO 0) :=  '0' & in1_alu_temp(31 DOWNTO 1);
+						i := i + 1;
+					END LOOP;
+				out_alu <= in1_alu_temp;
+				
+				WHEN "1101" => --SRA
+				i := 0;
+				in1_alu_temp := in1_alu;
+					WHILE i < TO_INTEGER(IEEE.NUMERIC_STD.UNSIGNED(in2_alu(4 DOWNTO 0))) LOOP
+						in1_alu_temp(31 DOWNTO 0) :=  in1_alu_temp(31 DOWNTO 31) & in1_alu_temp(31 DOWNTO 1);
+						i := i + 1;
+					END LOOP;
+				out_alu <= in1_alu_temp;
+				WHEN OTHERS => NULL;
+			END CASE;
+		END PROCESS;
 END comportamento;
